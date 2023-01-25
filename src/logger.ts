07a1225539e6ec3e08ad0d8e38c015444274eb1b
kesha1225 <samedov03@mail.ru>
2023-01-25 13:53:50 +0300

    fix

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,9 @@
-import { Logger } from '@nestjs/common';
+import { ConsoleLogger } from '@nestjs/common';
 import * as fs from 'fs';
 
 const logFile = "./apilog.log"
 
-export class FileLogger extends Logger {
+export class FileLogger extends ConsoleLogger {
   error(message: string, trace: string) {
     fs.writeFileSync(logFile, `${message} ${trace}`);
     super.error(message, trace);
